87d68705ee02e30936131b71f1fffd6e618a9ec2
kylmp <14829626+kylmp@users.noreply.github.com>
2025-03-15 01:33:53 +0900

    fix: bug with command match after making edits to engine.rs2 (#30)

--- client/matching/matchers/commandMatcher.js
+++ client/matching/matchers/commandMatcher.js
@@ -1,14 +1,15 @@
 const identifierCache = require("../../cache/identifierCache");
 const matchType = require("../matchType");
 const { reference, declaration } = require("../../utils/matchUtils");
+const { TRIGGER_LINE } = require("../../enum/regex");
 
 /**
  * Looks for matches of known engine commands
  */ 
 function commandMatcher(context) {
   const command = identifierCache.get(context.word.value, matchType.COMMAND);
-  if (command && context.prevChar !== '[') {
-    if (context.uri.fsPath.includes("engine.rs2")) {
+  if (command) {
+    if (context.uri.fsPath.includes("engine.rs2") && TRIGGER_LINE.test(context.line.text) && context.word.index === 1) {
       return declaration(matchType.COMMAND);
     }
     if (command.signature.params.length > 0 && context.nextChar !== '('){
